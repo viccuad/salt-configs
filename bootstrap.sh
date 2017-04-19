aed2983970d3ebcf8fdb9017139a99f17cce77f8
Víctor Cuadrado Juan <me@viccuad.me>
2017-04-19 21:21:37 +0200

    Add --symlink and --help to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,31 @@
 #! /usr/bin/env bash
 
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+    case $key in
+        -s|--symlink)
+            echo "symlinking"
+            ln -s -f "$(pwd)"/salt /srv/salt
+            ln -s -f "$(pwd)"/pillar /srv/pillar
+            ln -s -f "$(pwd)"/etc/minion /etc/salt/minion
+            shift # past argument
+            ;;
+        -h|--help)
+            echo "Usage: $0 [-s | --symlink]  [-h | --help]"
+            echo "Bootstrap all needed configuration, dependencies, and unitfiles for Salt"
+            echo "Possible options:"
+            echo "    symlink     Symlink salt configurations to this system"
+            echo "    help        Show this message"
+            exit
+            ;;
+        *)
+            # unknown option
+            ;;
+    esac
+    shift # past argument or value
+done
+
 # install gitpython instead of pygit2 to be able to use gitfs
 # See https://github.com/libgit2/pygit2/issues/644
 apt-get -y update
